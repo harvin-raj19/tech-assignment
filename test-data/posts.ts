dfacbf49062cd6a1c02656c8f395b53f8eeb22e0
Harvinraj <92132197+harvin-raj19@users.noreply.github.com>
2026-08-20 20:51:12 +0800

    add api test

---
+++ test-data/posts.ts
@@ -0,0 +1,9 @@
+export const newPost = {
+    title: 'QA Tech Assignment',
+    body: 'This is a test post for the QA Tech Assignment.',
+    userId: 192
+};
+
+export const updatedPost = {
+    body: 'This is an updated test post for the QA Tech Assignment.'
+};
